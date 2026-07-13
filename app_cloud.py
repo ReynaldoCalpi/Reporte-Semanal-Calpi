fc547f55ff24db9da69ef6ba01496129e8b8f557
ReynaldoCalpi <isma0806.rr@gmail.com>
2026-07-13 13:56:41 -0600

    app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -85,7 +85,7 @@ def cargar_datos():
     
     # Aquí pegas el link de tu Google Sheet (entre comillas)
     # worksheet="NombreDeTuHoja" es el nombre de la pestaña abajo en el Excel
-    df = conn.read(spreadsheet="# Cambia tu línea 88 por esta:
+    df = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1ozc9yAbVZ3vEhjJEOuQd2D14vhFd7JFSf6D8Jr2R-OQ/edit", worksheet="DATA")
 df = conn.read(worksheet="DATA")
     return df
 
