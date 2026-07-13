bed5f121938d7004f6953579d0214e12b6487ec2
ReynaldoCalpi <isma0806.rr@gmail.com>
2026-07-13 14:32:52 -0600

    app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -30,7 +30,7 @@ for ruta in rutas_logo:
 if logo_encontrado:
     st.sidebar.image("https://github.com/ReynaldoCalpi/Reporte-Semanal-Calpi/blob/main/logo.jpg", use_container_width=True)
 else:
-    st.sidebar.image("https://cdn-icons-png.flaticon.com/512/3303/3303038.png", width=70)
+    st.sidebar.image("https://github.com/ReynaldoCalpi/Reporte-Semanal-Calpi/blob/main/logo.jpg", width=70)
     st.sidebar.warning(f"⚠️ No Logo")
 
 st.sidebar.header("Panel de Control")
