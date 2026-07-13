1719b95478eeb1dd8f3af6fe1a16d33f72afe9bb
ReynaldoCalpi <isma0806.rr@gmail.com>
2026-07-13 14:43:51 -0600

    app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -88,6 +88,7 @@ df_master = cargar_datos()
 if df_master.empty:
     st.warning("⚠️ No se encontraron datos consolidados. Presiona el botón de 'Consolidar Nuevos Reportes' a la izquierda.")
 else:
+    st.logo("https://raw.githubusercontent.com/ReynaldoCalpi/Reporte-Semanal-Calpi/main/logo.jpg")
     st.title("📊 Reporte Semanal - Transportes Calpi")
     st.markdown("---")
 
